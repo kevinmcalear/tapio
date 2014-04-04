5ea3bc6f77294ab51cb89f30492c6b7f1f0c4bbd
kevinmcalear <kevin.mcalear@gmail.com>
2014-04-04 14:43:45 -0400

    starting tapio

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tapio::Application.config.secret_key_base = '6d635d0ee7357ef34e14b509b664bd76f5473f67aa1ab1b82c456a1ab802a8c2d56fe734b65b9759f8d9e3a981a294ec06137fa088e4950b5a60d755c448ab79'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Tapio::Application.config.session_store :cookie_store, key: '_tapio_session'
